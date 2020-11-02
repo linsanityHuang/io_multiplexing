ba314914c1b75df4d34e56b4d1beea87d453acaa
linsanityHuang <js_huang2018@163.com>
2020-11-02 23:30:13 +0800

    add common.h

---
+++ common.h
@@ -0,0 +1,87 @@
+#include <sys/types.h>  /* basic system data types */
+#include <sys/socket.h> /* basic socket definitions */
+#include <sys/time.h>   /* timeval{} for select() */
+#include <time.h>       /* timespec{} for pselect() */
+#include <netinet/in.h> /* sockaddr_in{} and other Internet defns */
+#include <arpa/inet.h>  /* inet(3) functions */
+#include <errno.h>
+#include <fcntl.h> /* for nonblocking */
+#include <netdb.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h> /* for S_xxx file mode constants */
+#include <sys/uio.h>  /* for iovec{} and readv/writev */
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/un.h> /* for Unix domain sockets */
+
+#include <sys/select.h> /* for convenience */
+#include <sys/sysctl.h>
+#include <poll.h>    /* for convenience */
+#include <strings.h> /* for convenience */
+#include <sys/ioctl.h>
+#include <pthread.h>
+
+#define SERV_PORT 43211
+#define MAXLINE 4096
+#define LISTENQ 1024
+#define BUFFER_SIZE 4096
+
+void make_nonblocking(int fd);
+
+void make_nonblocking(int fd)
+{
+    fcntl(fd, F_SETFL, O_NONBLOCK);
+}
+
+void error(int, int, char *);
+
+void error(int exit_code, int err_no, char *msg)
+{
+    if (err_no == 0)
+    {
+        fprintf(stderr, "%s\n", msg);
+    }
+    else
+    {
+        fprintf(stderr, "%s: %s\n", msg, strerror(err_no));
+    }
+    exit(exit_code);
+}
+
+int tcp_nonblocking_server_listen(int port);
+
+int tcp_nonblocking_server_listen(int port)
+{
+    int listenfd;
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    make_nonblocking(listenfd);
+
+    struct sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(port);
+
+    int on = 1;
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+    int rt1 = bind(listenfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (rt1 < 0)
+    {
+        error(1, errno, "bind failed");
+    }
+
+    int rt2 = listen(listenfd, LISTENQ);
+    if (rt2 < 0)
+    {
+        error(1, errno, "listen failed");
+    }
+
+    signal(SIGPIPE, SIG_IGN);
+
+    return listenfd;
+}
